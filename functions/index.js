1af065edb6e8716622879150e2b672a4c33fd574
Michael Hanscom <michaelrhanscom@Michaels-MacBook-Air.local>
2017-11-01 23:01:59 -0400

    up to speed

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
 const functions = require('firebase-functions');
+const express = require('express');
 
+const app = express();
+app.get('/timestamp', (request, response) => {
+    response.send(`${Date.now()}`);
+});
+
+app.get('/timestamp', (request, response) => {
+    response.set('Cache-Control','public, max-age=300, s-maxage=600');
+    response.send(`${Date.now()}`);
+});
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.app = functions.https.onRequest(app);
