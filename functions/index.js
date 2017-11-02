27b668134e8e21736157420d98e7938f9088e429
Michael Hanscom <michaelrhanscom@Michaels-MacBook-Air.local>
2017-11-02 13:16:16 -0400

    updating to josh's version

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,34 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
 const express = require('express');
+const engines = require('consolidate');
+
+const firebaseApp = firebase.initializeApp(
+    functions.config().firebase
+);
+
+function getvalue() {
+    const ref = firebaseApp.database().ref();
+    return ref.once('value').then(snap => snap.val());
+}
 
 const app = express();
-app.get('/timestamp', (request, response) => {
-    response.send(`${Date.now()}`);
+app.engine('hbs', engines.handlebars);
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.get('/', (request, response) => {
+    response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    getvalue().then(value => {
+        response.render('index', { value });
+    });
 });
 
-app.get('/timestamp', (request, response) => {
-    response.set('Cache-Control','public, max-age=300, s-maxage=600');
-    response.send(`${Date.now()}`);
+app.get('/value.json', (request, response) => {
+    response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    getvalue().then(value => {
+        response.json(value);
+    });
 });
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
 exports.app = functions.https.onRequest(app);
