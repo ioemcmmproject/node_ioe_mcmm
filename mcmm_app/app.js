2daca3b89f30b35771ece1e07345c5c029c1fbb2
Michael Hanscom <michaelrhanscom@Michaels-MacBook-Air.local>
2017-11-06 18:22:00 -0500

    moved index js to app js

--- mcmm_app/app.js
+++ mcmm_app/app.js
@@ -44,3 +44,40 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+/**const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
+const express = require('express');
+const engines = require('consolidate');
+
+const firebaseApp = firebase.initializeApp(
+    functions.config().firebase
+);
+
+function getvalue() {
+    const ref = firebaseApp.database().ref();
+    return ref.once('value').then(snap => snap.val());
+}
+
+const app = express();
+app.engine('hbs', engines.handlebars);
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.get('/', (request, response) => {
+    response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    getvalue().then(value => {
+        response.render('index', { value });
+    });
+});
+
+app.get('/value.json', (request, response) => {
+    response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    getvalue().then(value => {
+        response.json(value);
+    });
+});
+
+exports.app = functions.https.onRequest(app);
+ */
