56c90556a8861499cce9d4e42c8ed693dfa41af5
Michael Hanscom <michaelrhanscom@Michaels-MacBook-Air.local>
2017-11-07 13:23:36 -0500

    incorporated josh's code

--- mcmm_app/app.js
+++ mcmm_app/app.js
@@ -1,29 +1,54 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
+const engines = require('consolidate');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+//firebase setup
+const firebaseApp = firebase.initializeApp(
+    functions.config().firebase
+);
+
+function getvalue() {
+    const ref = firebaseApp.database().ref();
+    return ref.once('value').then(snap => snap.val());
+}
+
 // view engine setup
+app.engine('hbs', engines.handlebars);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+app.get('/', (request, response) => {
+    response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    getvalue().then(value => {
+        response.render('index', { value });
+    });
+});
+
+app.get('/value.json', (request, response) => {
+    response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    getvalue().then(value => {
+        response.json(value);
+    });
+});
+
+exports.app = functions.https.onRequest(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,39 +70,3 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-
-/**const functions = require('firebase-functions');
-const firebase = require('firebase-admin');
-const express = require('express');
-const engines = require('consolidate');
-
-const firebaseApp = firebase.initializeApp(
-    functions.config().firebase
-);
-
-function getvalue() {
-    const ref = firebaseApp.database().ref();
-    return ref.once('value').then(snap => snap.val());
-}
-
-const app = express();
-app.engine('hbs', engines.handlebars);
-app.set('views', './views');
-app.set('view engine', 'hbs');
-
-app.get('/', (request, response) => {
-    response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    getvalue().then(value => {
-        response.render('index', { value });
-    });
-});
-
-app.get('/value.json', (request, response) => {
-    response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    getvalue().then(value => {
-        response.json(value);
-    });
-});
-
-exports.app = functions.https.onRequest(app);
- */
