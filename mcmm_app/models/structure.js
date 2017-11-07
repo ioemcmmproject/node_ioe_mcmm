05f91abb0d306ff24e8211e3ad3fd0720dc1196f
Michael Hanscom <michaelrhanscom@Michaels-MacBook-Air.local>
2017-11-07 16:06:22 -0500

    continuing to build structure schema

--- mcmm_app/models/structure.js
+++ mcmm_app/models/structure.js
@@ -6,8 +6,19 @@ function getvalue() {
     const neighborhood = firebaseApp.database().ref();
     const architect = firebaseApp.database().ref();
     const decade = firebaseApp.database().ref();
+    const btype = firebaseApp.database().ref();
     //return ref.once('value').then(snap => snap.val());
 }  
 
-// var structure = 
+var StructureSchema = {
+    "type": 'object',
+    "properties": {
+        "neighborhood": {
+            "type": "string"
+        },
+        "architect": {"type": "string"},
+        "decade": {"type": "string"},
+        "btype": {"type": "string"}
+    },
+};
 // here is where we should put the if statement that uses the key that we need to decode
